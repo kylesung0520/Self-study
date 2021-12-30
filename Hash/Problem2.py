9334aa6d9439acdac4ed0b4c11f70e6398e045b3
ChangHoSung <84298467+kylesung0520@users.noreply.github.com>
2021-12-29 22:29:31 -0500

    Problem2

--- Hash/Problem2.py
+++ Hash/Problem2.py
@@ -1,6 +1,5 @@
-# Imported from leetcode
+# leetcode problem
 # Implement the class SubrectangleQueries which receives a rows x cols rectangle as a matrix of integers in the constructor and supports two methods:
-
 
 # 1. updateSubrectangle(int row1, int col1, int row2, int col2, int newValue)
 # Updates all values with newValue in the subrectangle whose upper left coordinate is (row1,col1) and bottom right coordinate is (row2,col2).

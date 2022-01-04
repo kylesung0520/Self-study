fad58252e49143cac3470c69fa23ab66136e6586
ChangHoSung <84298467+kylesung0520@users.noreply.github.com>
2022-01-03 22:58:41 -0500

    Problem4

---
+++ Problem4.py
@@ -0,0 +1,33 @@
+#leetcode problem
+
+# You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+# Find two lines that together with the x-axis form a container, such that the container contains the most water.
+# Return the maximum amount of water a container can store.
+
+# Input: height = [1,8,6,2,5,4,8,3,7]
+# Output: 49
+# Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        maxWater = 0
+        head = 0
+        tail = len(height) - 1
+        for index in range(len(height)):
+            width = abs(head - tail)
+            if height[head] < height[tail]:
+                tmp = width * height[head]
+                head += 1
+            else:
+                tmp = width * height[tail]
+                tail -= 1
+            if tmp > maxWater:
+                maxWater = tmp
+        return maxWater
+
+# Solved this problem using two pointer approach with O(n) time complexity and O(1) space complexity
+# But it can be solved by Brute Force algo as well, but it takes O(n^2) time complexity but same space complexity
